ed538499bb5b770b512d41c8bb4f0fd5af712d35
Layth-ele <layth.ele@gmail.com>
2016-11-17 16:22:32 -0800

    creat Montessori_Schedule widget

--- themes/montessori-theme/about.php
+++ themes/montessori-theme/about.php
@@ -1,23 +1,29 @@
 <?php
 /**
  * The template for displaying all pages.
- * Template Name: About Page
+ * Template Name: about
  * @package montessori_Theme
  */
 
 get_header(); ?>
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
 			<?php while ( have_posts() ) : the_post(); ?>
-
-
-
-			<?php endwhile; // End of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+				<div class="">
+					<?php if ( has_post_thumbnail() ) : ?>
+						<?php the_post_thumbnail( 'large' ); ?>
+							<?php endif; ?>
+				</div>
+				<section class="">
+					<?php the_title( '<h1 class="">', '</h1>' ); ?>
+						<?php the_content(); ?>
+							<p class="">
+								<?php echo CFS()->get('description'); ?>
+							</p>
+				</section>
+				<?php endwhile; // End of the loop. ?>
+		</main>
+		<!-- #main -->
+	</div>
+	<!-- #primary -->
+	<?php get_footer(); ?>
