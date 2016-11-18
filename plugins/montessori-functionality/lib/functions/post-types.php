64914ea55823a4de23c8d10cff698fd28e83838f
yousefhad <yousefhad@yahoo.com>
2016-11-17 21:55:37 -0800

    Footer CSS,HTML Mobile size

--- plugins/montessori-functionality/lib/functions/post-types.php
+++ plugins/montessori-functionality/lib/functions/post-types.php
@@ -6,3 +6,57 @@
  */
 
 // Add your custom post types here...
+// Register Custom Post Type
+function teachers_post_type() {
+
+	$labels = array(
+		'name'                  => 'Teachers',
+		'singular_name'         => 'Teacher',
+		'menu_name'             => 'Teacher',
+		'name_admin_bar'        => 'Teacher',
+		'archives'              => 'Teachers Archives',
+		'parent_item_colon'     => 'Parent Teacher:',
+		'all_items'             => 'All Teachers',
+		'add_new_item'          => 'Add New Teacher',
+		'add_new'               => 'Add New',
+		'new_item'              => 'New Teacher',
+		'edit_item'             => 'Edit Teacher',
+		'update_item'           => 'Update Teacher',
+		'view_item'             => 'View Teacher',
+		'search_items'          => 'Search Teacher',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into Teacher',
+		'uploaded_to_this_item' => 'Uploaded to this Teacher',
+		'items_list'            => 'Teachers list',
+		'items_list_navigation' => 'Teachers list navigation',
+		'filter_items_list'     => 'Filter Teachers list',
+	);
+	$args = array(
+		'label'                 => 'Teacher',
+		'description'           => 'This is the Teachers Post Type',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'author', 'thumbnail', 'custom-fields', ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-admin-users',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'post_type', $args );
+
+}
+add_action( 'init', 'teachers_post_type', 0 );
--- themes/montessori-theme/footer.php
+++ themes/montessori-theme/footer.php
@@ -11,19 +11,24 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="footer-blocks container">
+					<div class="footer-block-item1">
+
+			<ul>
+				<li><a href="#"> Disclaimer</a></li><li class="boarder1" ><a href="#">About</a></li><li class="boarder2"><a href="#">Contact us</a></li><li><a href="#"> Get involved</a></li>
+			</ul>
+					</div>
 				<div class="footer-block-item2">
-<p> Suite,0005550005 Colombia Street New Westmenester BC V3M 6V3
+<p><i class="fa fa-facebook" aria-hidden="true"></i><a href="">Follow us </a>
 <a href="info@newwestmontessori.ca">info@newwestmontessori.ca</a> </p>
 
 				</div>
 
-
-
 				<div class="footer-block-item3">
 
-				<h4> &copy 2016 New Westminster Montessri Socity. All right reserved</h4>
+				<p> &copy 2016 New Westminster Montessri Socity. All right reserved</p>
 
 				</div><!-- .site-info -->
+
 			</div>
 		</div><!-- #page -->
 </footer><!-- #colophon -->
