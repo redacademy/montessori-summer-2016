2a5cc7b73b456565aaa5058c75a467e6cfe99d64
Layth-ele <layth.ele@gmail.com>
2016-11-18 17:00:24 -0800

    add custom field loop to about page

--- themes/montessori-theme/footer.php
+++ themes/montessori-theme/footer.php
@@ -4,42 +4,22 @@
  *
  * @package Montessori_Theme
  */
-
 ?>
-
 			<div><!-- #content -->
-
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="footer-blocks container">
 					<div class="footer-block-item1">
-
 			<ul>
-				<li><a href="#"> Disclaimer</a></li><li class="boarder1" ><a href="#">About</a></li><li class="boarder2"><a href="#">Contact us</a></li><li><a href="#"> Get involved</a></li>
+				<li><a href="#"> Disclaimer</a></li><li class="boarder1" ><a href="#">About</a></li><li class="boarder2"><a href="#">Contact us</a></li><li><a href="#">Get involved</a></li>
 			</ul>
 					</div>
 				<div class="footer-block-item2">
-<<<<<<< HEAD
-<p> Suite,0005550005 Colombia Street New Westmenester BC V3M 6V3
-=======
 <p><i class="fa fa-facebook" aria-hidden="true"></i><a href="">Follow us </a>
->>>>>>> d47e8ff1f2ad0ba21f083a78de23017f6f2358ad
 <a href="info@newwestmontessori.ca">info@newwestmontessori.ca</a> </p>
-
-				</div>
-
+    	</div>
 				<div class="footer-block-item3">
-
 				<p> &copy 2016 New Westminster Montessri Socity. All right reserved</p>
-
-<<<<<<< HEAD
-				<div class="footer-block-item3">
-
-				<h4> &copy 2016 New Westminster Montessri Socity. All right reserved</h4>
-
-=======
->>>>>>> d47e8ff1f2ad0ba21f083a78de23017f6f2358ad
 				</div><!-- .site-info -->
-
 			</div>
 		</div><!-- #page -->
 </footer><!-- #colophon -->
