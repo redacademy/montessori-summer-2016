867af0ec8d2b8820cf70b10cda153ecc90a95cf1
yousefhad <yousefhad@yahoo.com>
2016-11-19 01:51:42 -0800

    link the logo to the Front-Page

--- themes/montessori-theme/header.php
+++ themes/montessori-theme/header.php
@@ -20,15 +20,17 @@
 			<header id="masthead" class="site-header" role="banner">
 <div class="main-navigation-container	">
 				<section class="montessori-logo">
-		<img src="<?php bloginfo('template_directory'); ?>/images/logo/montessori-logo.SVG" alt="montessori logo"/>
+
+					<div class="site-branding">
+						<div class="logo">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/logo/montessori-logo.SVG" alt="montessori logo"/>	
+									</a>
+</div>
 	</section>
 	<nav id="site-navigation" class="main-navigation" role="navigation">
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-
 </div>
 					<?php if ( is_page_template( 'about.php' )):?>
-
-
 						<?php
 						wp_nav_menu( array(
 							'theme_location' => 'about',
@@ -36,11 +38,8 @@
 							'menu_class'=> 'secondary'
 						) );
 						?>
-
 					<?php endif; ?>
-
 					<?php if ( is_page_template( 'montessori-page.php' )):?>
-
 						<?php
 						wp_nav_menu( array(
 							'theme_location' => 'montessori',
@@ -48,9 +47,7 @@
 							'menu_class'=> 'secondary'
 						) );
 						?>
-
 					<?php endif; ?>
-
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
 
