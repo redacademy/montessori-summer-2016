0d12ac4899a96da45cb61e2a70a82321afc73b43
yousefhad <yousefhad@yahoo.com>
2016-11-20 01:06:23 -0800

    create new custom field and get the content

--- plugins/montessori-functionality/lib/functions/post-types.php
+++ plugins/montessori-functionality/lib/functions/post-types.php
@@ -170,3 +170,58 @@ function materials_post_type() {
 
 }
 add_action( 'init', 'materials_post_type', 0 );
+
+// Register Custom Post Type
+function philosophy_post_type() {
+
+	$labels = array(
+		'name'                  => _x( 'Philosophys', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Philosophy', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Philosophy', 'text_domain' ),
+		'name_admin_bar'        => __( 'Philosophy', 'text_domain' ),
+		'archives'              => __( 'Philosophy Archives', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Philosophy:', 'text_domain' ),
+		'all_items'             => __( 'All Philosophys', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Philosophy', 'text_domain' ),
+		'add_new'               => __( 'Add New Philosophy', 'text_domain' ),
+		'new_item'              => __( 'New Philosophy', 'text_domain' ),
+		'edit_item'             => __( 'Edit Philosophy', 'text_domain' ),
+		'update_item'           => __( 'Update Philosophy', 'text_domain' ),
+		'view_item'             => __( 'View Philosophy', 'text_domain' ),
+		'search_items'          => __( 'Search Philosophy', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into Philosophy', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Philosophy', 'text_domain' ),
+		'items_list'            => __( 'Philosophys list', 'text_domain' ),
+		'items_list_navigation' => __( 'Philosophys list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter Philosophys list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Philosophy', 'text_domain' ),
+		'description'           => __( 'Our Philosophy', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'custom-fields', ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-format-status',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'philosophy', $args );
+
+}
+add_action( 'init', 'philosophy_post_type', 0 );
--- themes/montessori-theme/montessori-page.php
+++ themes/montessori-theme/montessori-page.php
@@ -85,4 +85,44 @@
 
 </section>
 
+
+<section id="philosophys" class="philosophy-section mobile-container">
+                  <ul class="main-materials">
+                    <h1 id="philosophy" >philosophy</h1>
+                    <?php
+                      $args = array(
+                        'post_type' => 'philosophy',
+                        'numberposts' => 6,
+                        'order' => 'ASC',
+                        );
+                      $philosophy_post = get_posts($args);
+                      ?>
+                      <?php foreach($philosophy_post as $post) : setup_postdata( $post); ?>
+                        <div class="">
+                          <!-- <a href="#<?php the_title();?>" class="">
+                            <img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-".$post->post_name.".svg"?>">
+                          </a> -->
+
+                          <h1><?php the_title();?></h1>
+                        </div>
+                        <div id="<?php the_title();?>" class="philosophy">
+                        </div>
+                        <div>
+                            <?php if ( has_post_thumbnail() ) : ?>
+                            </div>
+                            <div>
+                                <?php the_post_thumbnail( 'original' ); ?>
+                              </div>
+                            <?php endif; ?>
+<div>
+                            <p><?php the_content(); ?></p>
+
+                        </div>
+                      </li>
+                    <?php endforeach; wp_reset_postdata(); ?>
+                  </ul>
+                </div>
+
+</section>
+
 <?php get_footer();?>
