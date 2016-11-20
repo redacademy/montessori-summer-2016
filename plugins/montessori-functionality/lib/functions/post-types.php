32e9cedd6266e91b955805fe1c5b7386440c2920
yousefhad <yousefhad@yahoo.com>
2016-11-19 20:29:13 -0800

    Create Custom Post Type for Materials and History

--- plugins/montessori-functionality/lib/functions/post-types.php
+++ plugins/montessori-functionality/lib/functions/post-types.php
@@ -6,28 +6,26 @@
  */
 
 // Add your custom post types here...
-if ( ! function_exists('school_post_type') ) {
-
 // Register Custom Post Type
 function school_post_type() {
 
 	$labels = array(
 		'name'                  => 'Schools',
 		'singular_name'         => 'School',
-		'menu_name'             => 'Schools',
+		'menu_name'             => 'School',
 		'name_admin_bar'        => 'School',
-		'archives'              => 'School Archives',
+		'archives'              => 'Schools Archives',
 		'parent_item_colon'     => 'Parent School:',
 		'all_items'             => 'All Schools',
 		'add_new_item'          => 'Add New School',
-		'add_new'               => 'New School',
+		'add_new'               => 'Add New School',
 		'new_item'              => 'New School',
 		'edit_item'             => 'Edit School',
 		'update_item'           => 'Update School',
 		'view_item'             => 'View School',
-		'search_items'          => 'Search Schools',
-		'not_found'             => 'No Schools found',
-		'not_found_in_trash'    => 'No Schools found in Trash',
+		'search_items'          => 'Search School',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
 		'featured_image'        => 'Featured Image',
 		'set_featured_image'    => 'Set featured image',
 		'remove_featured_image' => 'Remove featured image',
@@ -40,9 +38,9 @@ function school_post_type() {
 	);
 	$args = array(
 		'label'                 => 'School',
-		'description'           => 'information about the school',
+		'description'           => 'montesori school',
 		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', 'custom-fields', ),
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'custom-fields', ),
 		'taxonomies'            => array( 'category', 'post_tag' ),
 		'hierarchical'          => false,
 		'public'                => true,
@@ -63,48 +61,49 @@ function school_post_type() {
 }
 add_action( 'init', 'school_post_type', 0 );
 
-}
+
 // Register Custom Post Type
-function teachers_post_type() {
+function history_post_type() {
 
 	$labels = array(
-		'name'                  => 'Teachers',
-		'singular_name'         => 'Teacher',
-		'menu_name'             => 'Teachers',
-		'name_admin_bar'        => 'Teacher',
-		'archives'              => 'Teacher Archives',
-		'parent_item_colon'     => 'Parent Teacher:',
-		'all_items'             => 'All Teachers',
-		'add_new_item'          => 'Add New Teacher',
-		'add_new'               => 'New Teacher',
-		'new_item'              => 'New Teacher',
-		'edit_item'             => 'Edit Teacher',
-		'update_item'           => 'Update Teacher',
-		'view_item'             => 'View Teacher',
-		'search_items'          => 'Search Teachers',
-		'not_found'             => 'No Teachers found',
-		'not_found_in_trash'    => 'No Teachers found in Trash',
+		'name'                  => 'Historys',
+		'singular_name'         => 'History',
+		'menu_name'             => 'History',
+		'name_admin_bar'        => 'History',
+		'archives'              => 'History Archives',
+		'parent_item_colon'     => 'Parent History:',
+		'all_items'             => 'All Historys',
+		'add_new_item'          => 'Add New History',
+		'add_new'               => 'Add New',
+		'new_item'              => 'New History',
+		'edit_item'             => 'Edit History',
+		'update_item'           => 'Update History',
+		'view_item'             => 'View History',
+		'search_items'          => 'Search History',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
 		'featured_image'        => 'Featured Image',
 		'set_featured_image'    => 'Set featured image',
 		'remove_featured_image' => 'Remove featured image',
 		'use_featured_image'    => 'Use as featured image',
-		'insert_into_item'      => 'Insert into Teacher',
-		'uploaded_to_this_item' => 'Uploaded to this Teacher',
-		'items_list'            => 'Teachers list',
-		'items_list_navigation' => 'Teachers list navigation',
-		'filter_items_list'     => 'Filter Teachers list',
+		'insert_into_item'      => 'Insert into History',
+		'uploaded_to_this_item' => 'Uploaded to this History',
+		'items_list'            => 'Historys list',
+		'items_list_navigation' => 'Historys list navigation',
+		'filter_items_list'     => 'Filter historys list',
 	);
 	$args = array(
-		'label'                 => 'Teacher',
-		'description'           => 'teacher information pages.',
+		'label'                 => 'History',
+		'description'           => 'Our history Description',
 		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', ),
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'custom-fields', ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
 		'hierarchical'          => false,
 		'public'                => true,
 		'show_ui'               => true,
 		'show_in_menu'          => true,
 		'menu_position'         => 5,
-		'menu_icon'             => 'dashicons-admin-users',
+		'menu_icon'             => 'dashicons-album',
 		'show_in_admin_bar'     => true,
 		'show_in_nav_menus'     => true,
 		'can_export'            => true,
@@ -113,7 +112,61 @@ function teachers_post_type() {
 		'publicly_queryable'    => true,
 		'capability_type'       => 'page',
 	);
-	register_post_type( 'teacher', $args );
+	register_post_type( 'history', $args );
 
 }
-add_action( 'init', 'teachers_post_type', 0 );
+add_action( 'init', 'history_post_type', 0 );
+// Register Custom Post Type
+function materials_post_type() {
+
+	$labels = array(
+		'name'                  => 'Material',
+		'singular_name'         => 'Material',
+		'menu_name'             => 'Material',
+		'name_admin_bar'        => 'Material',
+		'archives'              => 'Material Archives',
+		'parent_item_colon'     => 'Parent Material:',
+		'all_items'             => 'All Materials',
+		'add_new_item'          => 'Add New Material',
+		'add_new'               => 'Add New Material',
+		'new_item'              => 'New Material',
+		'edit_item'             => 'Edit Material',
+		'update_item'           => 'Update Material',
+		'view_item'             => 'View Material',
+		'search_items'          => 'Search Material',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into Material',
+		'uploaded_to_this_item' => 'Uploaded to this Material',
+		'items_list'            => 'Materials list',
+		'items_list_navigation' => 'Materials list navigation',
+		'filter_items_list'     => 'Filter Materials list',
+	);
+	$args = array(
+		'label'                 => 'Material',
+		'description'           => 'montesori materials',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'custom-fields', ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-art',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'materials', $args );
+
+}
+add_action( 'init', 'materials_post_type', 0 );
--- themes/montessori-theme/montessori-page.php
+++ themes/montessori-theme/montessori-page.php
@@ -7,54 +7,85 @@
  get_header();
 
 ?>
-<div class="container2">
-<section id="history" class="our-history">
 
-  		<!-- <?php while ( have_posts() ) : the_post(); ?>
-<div class="history">
-  			<h3><?php echo CFS()->get( 'our_history_title' ); ?></h3>
-      </div>
-      <div class="cfsBorder">
-  			<p><?php echo CFS()->get( 'our_history_description' ); ?></p>
-      </div>
-  			<h3><?php echo CFS()->get( 'our_history_content' ); ?></h3>
+        <main id="main" class="site-main" role="main">
 
+          <section id="history" class="history-section container">
+                            <ul class="main-history">
+                              <?php
+                                $args = array(
+                                  'post_type' => 'history',
+                                  'numberposts' => -1,
+                                  'order' => 'ASC',
+                                  );
+                                $history_post = get_posts($args);
+                                ?>
+                                <?php foreach($history_post as $post) : setup_postdata( $post); ?>
+                                  <div class="">
+                                    <!-- <a href="#<?php the_title();?>" class="">
+                                      <img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-".$post->post_name.".svg"?>">
+                                    </a> -->
+                                    <h1><?php the_title();?></h1>
+                                  </div>
+                                  <div id="<?php the_title();?>" class="history">
+                                  </div>
+                                  <div>
+                                      <?php if ( has_post_thumbnail() ) : ?>
+                                      </div>
+                                      <div>
+                                          <?php the_post_thumbnail( 'original' ); ?>
+                                        </div>
+                                      <?php endif; ?>
+          <div>
+                                      <p><?php the_content(); ?></p>
 
+                                  </div>
+                                </li>
+                              <?php endforeach; wp_reset_postdata(); ?>
+                            </ul>
+                          </div>
 
-  		<?php endwhile; // End of the loop. ?> -->
+          <h1>yousef2</h1>
 
+          </section>
 
-  						</div>
-              <h1>inhabitent journal</h1>
-              				<ul>
-              					<?php
-              					   $args = array( 'post_type' => 'school', 'posts_per_page' => 1);
-              					   $recent_posts = get_posts( $args ); // returns an array of posts
-              					?>
-              					<?php foreach ( $recent_posts as $post ) : setup_postdata( $post ); ?>
-              					<li>
-              						<div class="thumbnail-wrapper">
-              							<?php if ( has_post_thumbnail() ) : ?>
-              								<?php the_post_thumbnail( 'large' ); ?>
-                              <?php the_title(); ?>
-                              <?php the_author(); ?>
+<section id="materials" class="materials-section container">
+                  <ul class="main-materials">
+                    <?php
+                      $args = array(
+                        'post_type' => 'materials',
+                        'numberposts' => -1,
+                        'order' => 'ASC',
+                        );
+                      $material_post = get_posts($args);
+                      ?>
+                      <?php foreach($material_post as $post) : setup_postdata( $post); ?>
+                        <div class="">
+                          <!-- <a href="#<?php the_title();?>" class="">
+                            <img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-".$post->post_name.".svg"?>">
+                          </a> -->
+                          <h1><?php the_title();?></h1>
+                        </div>
+                        <div id="<?php the_title();?>" class="material">
+                        </div>
+                        <div>
+                            <?php if ( has_post_thumbnail() ) : ?>
+                            </div>
+                            <div>
+                                <?php the_post_thumbnail( 'original' ); ?>
+                              </div>
+                            <?php endif; ?>
+<div>
+                            <p><?php the_content(); ?></p>
 
-              							<?php endif; ?>
+                        </div>
+                      </li>
+                    <?php endforeach; wp_reset_postdata(); ?>
+                  </ul>
+                </div>
 
-              						</div>
-              					</li>
-              						<?php endforeach; wp_reset_postdata(); ?>
-              			 </ul>
+<h1>yousef2</h1>
+
 </section>
-
-</div>
-
-
-
-
-
-
-
-
 
 <?php get_footer();?>
