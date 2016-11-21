a370d61459ca43ea948d15814a59dc66428f8ecf
ahlam <a7sh_alhasan@yahoo.com>
2016-11-21 12:43:52 -0800

    change the involve scss

--- themes/montessori-theme/involved.php
+++ themes/montessori-theme/involved.php
@@ -8,26 +8,42 @@ get_header(); ?>
 
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
+
  <?php
 $args = array(
     'post_type' => 'post',
  'showposts' => 3,
 );
 $query = new WP_Query( $args  );
+
 if ( $query->have_posts() ) :  ?>
+
     <?php while ($query-> have_posts() )  : $query->the_post(); ?>
 
 
+<section class="involved-wrapper container">
+  <div class="green-btn">
+<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+</div>
+<?php the_content();?>
+<?php if (has_post_thumbnail( )): ?>
+         		<?php the_post_thumbnail( 'Normal' ); ?>
 
+ <a class="moretag" href="<?php the_permalink(); ?>">Exprsee your Intrest</a>
+          <?php endif ; ?>
     <?php endwhile;  ?>
 
 <?php endif; ?>
 
 
 
+</section>
 
    </main><!-- #main -->
  </div><!-- #primary -->
+
+
+  <!-- This is for support now-->
 
 
 
