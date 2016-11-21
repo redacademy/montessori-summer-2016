c255c641de2ee462a3ef90496d89212618e9d093
Ayah Al-Hasan <ayah.alhasan@gmail.com>
2016-11-21 12:20:13 -0800

    style features news flipper

--- themes/montessori-theme/front-page.php
+++ themes/montessori-theme/front-page.php
@@ -9,34 +9,35 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+<!-- <div class="home container"> -->
 
       <section class="home-wrapper container">
 				<h2 class="site-title screen-reader-text">Montessori Society</h2>
         <img src="<?php bloginfo('template_directory'); ?>/images/header-pic2.jpg" alt="img" />
-<!-- this is for the welcoming  -->
-    <div class="container-wrapper">
-      <h2> <?php echo CFS()->get('banner_header'); ?> </h2>
-        <p><?php echo CFS()->get('banner_paragraph') ?></p>
-        <a class="red-btn" href="<?php the_permalink(); ?>">Our Society Role</a>
-    </div>
-  </section>
+<!-- this is for the welcoming title  -->
+        <div class="container-wrapper">
+          <h2> <?php echo CFS()->get('banner_header'); ?> </h2>
+            <p><?php echo CFS()->get('banner_paragraph') ?></p>
+            <a class="red-btn" href="<?php echo esc_url(the_permalink()); ?>/about/">Our Society Role</a>
+        </div>
+      </section>
 
 <!-- this is for the quick links -->
     <section class="links-wrapper container">
       <h2> <?php echo CFS()->get('links_title'); ?> </h2>
 
         <article class="benifits">
-          <a class="green-btn" href="<?php the_permalink(); ?>">Benefits of Montessori</a>
+          <a class="green-btn" href="<?php  echo esc_url(the_permalink()); ?>/what-is-montessori/">Benefits of Montessori</a>
           <p><?php echo CFS()->get('contents'); ?></p>
         </article>
 
         <article class="nw-program">
-          <a class="green-btn" href="<?php the_permalink(); ?>">NW Program</a>
+          <a class="green-btn" href="<?php echo esc_url(the_permalink()); ?>/nw-program/">NW Program</a>
           <p><?php echo CFS()->get('nw_text'); ?></p>
         </article>
 
         <article class="involved">
-          <a class="red-btn" href="<?php the_permalink(); ?>">Get involved</a>
+          <a class="red-btn" href="<?php echo esc_url(the_permalink()); ?>/get-involved/">Get involved</a>
           <p><?php echo CFS()->get('get_text'); ?></p>
         </article>
 
@@ -51,22 +52,47 @@ get_header(); ?>
           <a class="green-btn" href="<?php the_permalink(); ?>">How $ is Spent</a>
       </div>
     </section>
+<!-- this is for the features news  -->
+    <section class="all-features container">
+      <h2> <?php echo CFS()->get('featured_title'); ?></h2>
 
-    <h2> <?php echo CFS()->get('featured_title'); ?></h2>
-    <section class="features-wrapper container">
-      <!-- <div class="features"> -->
+      <section class="features-wrapper container">
         <div class="features-flipper">
           <p class="front">
             <?php echo CFS()->get('features_front'); ?>
           </p>
           <p class="features-back">
-            <?php echo CFS()->get('features_back'); ?> <br>
+            <?php echo CFS()->get('features_back'); ?>
             <a class="green-btn" href="<?php the_permalink(); ?>">Read More</a>
           </p>
         </div>
-    <!-- </div> -->
-    </section>
+      </section>
+<!-- second flipper -->
 
+        <section class="features-wrapper container">
+            <div class="features-flipper">
+              <p class="front">
+                <?php echo CFS()->get('second_features_front'); ?>
+              </p>
+              <p class="features-back">
+                <?php echo CFS()->get('second_features_back'); ?>
+                <a class="green-btn" href="<?php the_permalink(); ?>">Read More</a>
+              </p>
+            </div>
+        </section>
+<!-- third flipper -->
+            <section class="features-wrapper container">
+                <div class="features-flipper">
+                  <p class="front">
+                    <?php echo CFS()->get('third_features_front'); ?>
+                  </p>
+                  <p class="features-back">
+                    <?php echo CFS()->get('third_features_back'); ?>
+                    <a class="green-btn" href="<?php the_permalink(); ?>">Read More</a>
+                  </p>
+                </div>
+              </section>
+</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
