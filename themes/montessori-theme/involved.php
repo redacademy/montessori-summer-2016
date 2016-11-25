7d53718d3230d77f43e038e8db8d6360f4ff6790
ahlam <a7sh_alhasan@yahoo.com>
2016-11-25 13:15:54 -0800

    style the mobile first in the page

--- themes/montessori-theme/involved.php
+++ themes/montessori-theme/involved.php
@@ -1,95 +1,89 @@
 <?php
 /**
 * The main template file.
-*template name: Get-involved
+*template name: get-involved
 * @package RED_Starter_Theme
 */
-get_header(); ?>
+get_header();
+ ?>
 
-    <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
-<section class="involved-wrapper ">
-<div class="main-what-new mob-container">
- <?php
-$args = array(
 
+
+          <!-- our involved section -->
+<section id="involved" class="involved-section mob-container ">
+<div class="main-involved-wrapper">
+
+
+    <?php
+$args = array(
     'post_type' => 'post',
     'show_per_page' => 1,
 );
 $query = new WP_Query( $args  );
+if ( $query-> have_posts() ) :
+  while ($query-> have_posts() )  : $query->the_post(); ?>
+   <div class="involved-title">
+      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+   </div>
+<?php endwhile; ?>
 
+<?php while ($query-> have_posts() )  : $query->the_post(); ?>
+<div class="involved-content">
+    <?php the_content('involved_content');?>
+  </div>
+    <div class= "involved-img" >
+      <?php output_image('involved_img'); ?>
 
-if ( $query-> have_posts() ) :  ?>
+  </div>
 
-    <?php while ($query-> have_posts() )  : $query->the_post(); ?>
+      <div class="button">
+        <a class="green-button" href="<?php echo esc_url(the_permalink()); ?>">Exprsee your intrest</a></div>
 
-
-
-  <div class="involved-title">
-
-     
-
-<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-
-
-<?php the_content();?>
-</div>
-
-<!-- <?php //if (has_post_thumbnail( )): ?> -->
-
-</div>
-
-
-
-    <php the_content();?>
-
-
-    <?php if (has_post_thumbnail()): ?>
-    <?php the_post_thumbnail('Normal'); ?>
-
-      <a class="green-btn" href="<?php echo esc_url(the_permalink()); ?>">Exprsee your intrest</a>
-
-    <?php endif; ?>
   <?php endwhile; ?>
 <?php endif; ?>
-
-
-
 <?php wp_reset_postdata(); ?>
 
-</section>
-</main><!-- #main -->
-</div><!-- #primary -->
 
 
-
-<!--This is for past Events  -->
-<!-- <div class="text-title">
-<h3>  <a class="text_post" href="<?php echo esc_url(the_permalink()); ?>">past Events </a> </h3>
-</div> -->
 
 <!-- This is for suppot now  & did you  and image -->
 <div class="text-shadow">
-<h2
-  <a class="text_post" href="<?php echo esc_url(the_permalink()); ?>">Support Now </a></div>
-<h2>
-  <a class="banner_post" href="<?php echo esc_url(the_permalink()); ?>">Did you Know.... </a>
+  <h2>Support Now</h2>
+</div>
+<div class="banner_post">
 
-  <div class="involve-page">
-           <?php if (has_post_thumbnail( )): ?>
-          		<?php the_post_thumbnail( 'large' ); ?>
-           <?php endif ; ?>
-         </div>
+  <h2>Did you Know.... </h2>
+  </div>
+<!-- this for support section -->
+<div support-containar>
+  <div class="support-img"><?php output_image('support_img'); ?>
+    <?php output_image('play_img'); ?>
+  </div>
+</div>
          <article class="involved">
-           <a class="green-btn" href="<?php echo esc_url(the_permalink()); ?>/get-involved/">your involvement+contribution</a>
+           <a class="circle-btn" href="<?php echo esc_url(the_permalink()); ?>/get-involved/">your involvement+contribution</a>
            <p><?php echo CFS()->get('get_text'); ?></p>
          </article>
 
-         <article class="involved">
-           <a class="red-btn" href="<?php echo esc_url(the_permalink()); ?>/get-involved/">voleenter</a>
-           <a class="red-btn" href="<?php echo esc_url(the_permalink()); ?>/get-involved/">make a donation</a>
+         <div>
+                     <a class="re-btn" href="<?php echo esc_url(the_permalink()); ?>/get-involved/">Voleenter</a>
+                   <a class="pink-btn" href="<?php echo esc_url(the_permalink()); ?>/get-involved/">Make a donation</a>
+          </div>
+
+         <article class="contact-container">
+              <div class="contact-person">
+            <a class="gre-btn" href="<?php echo esc_url(the_permalink()); ?>/get-involved/"in person ></a></div>
+            <div class="contact-online">
+              <a class="gre-btn" href="<?php echo esc_url(the_permalink()); ?>/get-involved/" online ></a><div>
+              <div class="contact-email">
+                  <a class="gre-btn" href="<?php echo esc_url(the_permalink()); ?>/get-involved/" email></a></div>
            <p><?php echo CFS()->get('get_text'); ?></p>
          </article>
+       </section>
+
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
+</main>
+</div>
