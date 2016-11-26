6f02ebd495bc261ba722e8e3f3fa20b968516f9f
Ayah Al-Hasan <ayah.alhasan@gmail.com>
2016-11-25 17:49:40 -0800

    small change in the page

--- themes/montessori-theme/front-page.php
+++ themes/montessori-theme/front-page.php
@@ -20,25 +20,24 @@ get_header(); ?>
         </div>
       </section>
 <!-- this is for the quick links -->
-<div class="link-title">
-  <h2> <?php echo CFS()->get('links_title'); ?> </h2>
-</div>
-
-    <section class="links-wrapper ">
-        <article class="benifits">
-          <a class="green-btn" href="<?php  echo esc_url(the_permalink()); ?>/what-is-montessori/">Benefits of Montessori</a>
-          <p><?php echo CFS()->get('contents'); ?></p>
-        </article>
-        <article class="nw-program-page">
-          <a class="green-btn" href="<?php echo esc_url(the_permalink()); ?>/nw-program/">NW Program</a>
-          <p><?php echo CFS()->get('nw_text'); ?></p>
-        </article>
-        <article class="involved">
-          <a class="red-btn" href="<?php echo esc_url(the_permalink()); ?>/get-involved/">Get involved</a>
-          <p><?php echo CFS()->get('get_text'); ?></p>
-        </article>
-    </section>
-    <!-- we need your support section -->
+    <div class="link-title">
+      <h2> <?php echo CFS()->get('links_title'); ?> </h2>
+    </div> 
+      <section class="links-wrapper ">
+          <article class="benifits">
+            <a class="green-btn" href="<?php  echo esc_url(the_permalink()); ?>/what-is-montessori/">Benefits of Montessori</a>
+            <p><?php echo CFS()->get('contents'); ?></p>
+          </article>
+          <article class="nw-program-page">
+            <a class="green-btn" href="<?php echo esc_url(the_permalink()); ?>/nw-program/">NW Program</a>
+            <p><?php echo CFS()->get('nw_text'); ?></p>
+          </article>
+          <article class="involved">
+            <a class="red-btn" href="<?php echo esc_url(the_permalink()); ?>/get-involved/">Get involved</a>
+            <p><?php echo CFS()->get('get_text'); ?></p>
+          </article>
+      </section>
+<!-- we need your support section -->
     <section class="support-wrapper ">
       <div class="information">
         <h2> <?php echo CFS()->get('support_title'); ?> </h2>
