9ede14d2c9ceb9559297a04701e0867ab777305f
Layth-ele <layth.ele@gmail.com>
2016-11-26 17:38:28 -0800

    add custom taxonomy to school custom post

--- plugins/montessori-functionality/lib/functions/taxonomies.php
+++ plugins/montessori-functionality/lib/functions/taxonomies.php
@@ -5,12 +5,7 @@
  * @link  http://codex.wordpress.org/Function_Reference/register_taxonomy
  */
 
-
-
-
-// Register Staff Taxonomy
-// Register Custom Taxonomy
-// Register Custom Taxonomy
+// Register staff categories
 function staff_category() {
 
 	$labels = array(
@@ -48,3 +43,44 @@ function staff_category() {
 
 }
 add_action( 'init', 'staff_category', 0 );
+
+
+
+// Register Custom school categories
+function school_categories() {
+
+	$labels = array(
+		'name'                       => _x( 'school categories', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'school category', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'school categories', 'text_domain' ),
+		'all_items'                  => __( 'All School categories', 'text_domain' ),
+		'parent_item'                => __( 'Parent School categories', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent School category:', 'text_domain' ),
+		'new_item_name'              => __( 'New School category Name', 'text_domain' ),
+		'add_new_item'               => __( 'Add New School category', 'text_domain' ),
+		'edit_item'                  => __( 'Edit School category', 'text_domain' ),
+		'update_item'                => __( 'Update School category', 'text_domain' ),
+		'view_item'                  => __( 'View School category', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate school categories with commas', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove school categories', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+		'popular_items'              => __( 'Popular School categories', 'text_domain' ),
+		'search_items'               => __( 'Search School categories', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+		'no_terms'                   => __( 'No school categories', 'text_domain' ),
+		'items_list'                 => __( 'School categories list', 'text_domain' ),
+		'items_list_navigation'      => __( 'School categories list navigation', 'text_domain' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'school categories', array( 'school' ), $args );
+
+}
+add_action( 'init', 'school_categories', 0 );
