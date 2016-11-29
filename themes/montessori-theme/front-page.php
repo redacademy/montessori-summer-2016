3f26df72d9cfe53be353c264edac9838866ba620
Ayah Al-Hasan <ayah.alhasan@gmail.com>
2016-11-28 23:24:47 -0800

    style features news

--- themes/montessori-theme/front-page.php
+++ themes/montessori-theme/front-page.php
@@ -39,46 +39,45 @@ get_header(); ?>
             <p><?php echo CFS()->get('get_text'); ?></p>
           </article>
       </section>
-<!-- we need your support section -->
-    <section class="support-wrapper ">
-      <div class="information">
-        <h2> <?php echo CFS()->get('support_title'); ?> </h2>
-        <p class="info"> <?php echo CFS()->get('first_support_paragraph'); ?> </p>
-        <div class="support-img-wrapper">
-           <img src="<?php bloginfo('template_directory'); ?>/images/Group-8.jpg" alt="image" />
-            <p class="image-text"> Your monthly contribution costs only two Lattes a month.</p>
-        </div>
-       <?php echo CFS()->get('second_support_paragraph'); ?>
-            <a class="red-btn" href="<?php echo esc_url(the_permalink()); ?>/get-involved/">Support Now</a>
-          <a class="green-btn" href="<?php echo esc_url(the_permalink()); ?>/nw-program/">How Money is Spent</a>
-      </div>
-    </section>
+      <!-- we need your support section -->
+          <section class="support-wrapper ">
+            <div class="information">
+              <h2> <?php echo CFS()->get('support_title'); ?> </h2>
+              <p class="info"> <?php echo CFS()->get('first_support_paragraph'); ?> </p>
 
+              <div class="support-img-wrapper">
+                <div> <img src="<?php bloginfo('template_directory'); ?>/images/Group-8.jpg" alt="image" /> </div>
+                <p class="image-text" >Your monthly contribution costs only two Lattes a month.</p>
+              </div>
+
+             <?php echo CFS()->get('second_support_paragraph'); ?>
+                  <a class="red-btn" href="<?php echo esc_url(the_permalink()); ?>/get-involved/">Support Now</a>
+                <a class="green-btn" href="<?php echo esc_url(the_permalink()); ?>/nw-program/">How Money is Spent</a>
+            </div>
+          </section>
 <!-- this is for the features news  -->
 
-<section class="all-features container">
-  <h2>Features News</h2>
-
-   <div class="features-wrapper">
-       <?php
-          $fields = CFS()->get('features_news'); ?>
-      <?php
-      foreach($fields as $field)
-          { ?>
-        <div class="features-flipper">
-          <div class="front">
-            <p><?php echo $field['front']; ?></p>
+    <section class="all-features container">
+        <h2>Features News</h2>
+         <div class="features-wrapper">
+             <?php
+                $fields = CFS()->get('features_news'); ?>
+            <?php
+            foreach($fields as $field)
+                { ?>
+              <div class="features-flipper">
+                <div class="front">
+                  <p><?php echo $field['front']; ?></p>
+                </div>
+                <div class="features-back">
+                  <p><?php echo $field['features_back']; ?></p>
+                  <a class="green-btn" href="<?php the_permalink(); ?>/get-involved/">Read Entry</a>
+                </div>
+              </div>
+               <?php
+                } ?>
           </div>
-          <div class="features-back">
-            <p><?php echo $field['features_back']; ?></p>
-            <a class="green-btn" href="<?php the_permalink(); ?>/get-involved/">Read Entry</a>
-
-          </div>
-        </div>
-         <?php
-          } ?>
-    </div>
-</section>
+    </section>
 
         </main><!-- #main -->
     </div><!-- #primary -->
